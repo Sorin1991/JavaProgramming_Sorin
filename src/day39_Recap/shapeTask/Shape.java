f2fe33c2dc23cc0e0271b26da21d5bfbc2478703
Sorin1991 <basco91@gmail.com>
2022-04-14 22:06:56 +0100

    Shape

--- src/day39_Recap/shapeTask/Shape.java
+++ src/day39_Recap/shapeTask/Shape.java
@@ -32,6 +32,15 @@ public class Shape {
     public double perimeter(){
         return 0;
     }
+
+    @Override
+    public String toString() {
+        return "Shape{" +
+                "name='" + name + '\'' +
+                ", area='" + area() + '\'' +
+                ", perimeter='" + perimeter() + '\'' +
+                '}';
+    }
 }
 
 /*
