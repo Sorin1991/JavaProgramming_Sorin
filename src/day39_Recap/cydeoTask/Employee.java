e227825d46c39611c66e77a75ba01d55bd655e46
Sorin1991 <basco91@gmail.com>
2022-04-15 11:00:13 +0100

    cydeoTask

--- src/day39_Recap/cydeoTask/Employee.java
+++ src/day39_Recap/cydeoTask/Employee.java
@@ -55,3 +55,17 @@ public class Employee extends Person{
                 '}';
     }
 }
+/*
+2. Create a subclass of Person named Employee:
+            Extra variables:
+                employeeId, jobTitle, salary
+
+            Encapsulate all the fields
+
+            Add a constructor to set all the fields
+
+            Methods:
+                work()
+                toString()
+ */
+
---
+++ src/day39_Recap/cydeoTask/Tester.java
@@ -0,0 +1,14 @@
+package day39_Recap.cydeoTask;
+
+public class Tester {
+}
+
+/*
+3. Create a sub class of Employee named Tester
+
+            Override the work method
+
+            Extra methods:
+                createTicket()
+
+ */
