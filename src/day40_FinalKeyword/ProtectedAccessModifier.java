9a412566f2c036a278fcd34bb48b76127931f00d
Sorin1991 <basco91@gmail.com>
2022-04-16 22:40:02 +0100

    Access Modifiers

--- src/day40_FinalKeyword/ProtectedAccessModifier.java
+++ src/day40_FinalKeyword/ProtectedAccessModifier.java
@@ -5,4 +5,12 @@ public class ProtectedAccessModifier {
     static String name1 = "Cydeo"; // default access modifier
     protected static String name2 = "WoodenSpoon"; // protected
 
+    static void method1(){
+
+    }
+
+    protected static void method2(){
+
+    }
+
 }
