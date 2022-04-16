a86540abc043a569c23a88e4a7fa2843a853b83d
Sorin1991 <basco91@gmail.com>
2022-04-16 23:34:03 +0100

    FinalKeyword

--- src/day40_FinalKeyword/Dog.java
+++ src/day40_FinalKeyword/Dog.java
@@ -1,6 +1,6 @@
 package day40_FinalKeyword;
 
-public class Dog extends Animal {
+public final class Dog extends Animal {
 
     public Dog(String name, String breed, char gender, String color, String size, int age) {
         super(name, breed, gender, color, size, age);
@@ -11,6 +11,10 @@ public class Dog extends Animal {
     }
 
     //public void drink(){ }// final methods can not be overridden
+
+    public  void bark(){
+        System.out.println(getName()+ " is barking");
+    }
 
 
 
