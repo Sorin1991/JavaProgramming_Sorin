bf020ce4b9e74495d27485d21f163b582a2e35be
Sorin1991 <basco91@gmail.com>
2022-04-23 10:43:49 +0100

    DisadvantageOfThrowsKeyword

--- src/day42_Exceptions/ThrowsKeyword2.java
+++ src/day42_Exceptions/ThrowsKeyword2.java
@@ -2,7 +2,7 @@ package day42_Exceptions;
 
 public class ThrowsKeyword2 {
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         System.out.println("Test started");
 
         System.out.println(8/0);
