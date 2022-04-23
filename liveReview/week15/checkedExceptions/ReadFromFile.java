229ed2642234c4bded0aa0c9f0f3041a9ad5345d
Sorin1991 <basco91@gmail.com>
2022-04-23 17:11:31 +0100

    Exceptions example

---
+++ liveReview/week15/checkedExceptions/ReadFromFile.java
@@ -0,0 +1,12 @@
+package week15.checkedExceptions;
+
+public class ReadFromFile {
+    public static void main(String[] args) {
+
+        // tell java the patch of the file
+
+        String filePath ="liveReview/week15/checkedExceptions/data.txt";
+
+
+    }
+}
