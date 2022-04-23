7e4e110580966156c341e01d156aaf0e689b41b3
Sorin1991 <basco91@gmail.com>
2022-04-23 16:40:36 +0100

    Exceptions example

---
+++ liveReview/week15/MultiCatchExamples.java
@@ -0,0 +1,19 @@
+package week15;
+
+public class MultiCatchExamples {
+    public static void main(String[] args) {
+
+
+        String word = "Java"; //length is 4
+
+        try{
+            System.out.println(word.substring(20)); // unchecked exception
+        }catch (StringIndexOutOfBoundsException e){
+            System.out.println(e.getClass().getSimpleName());
+        }catch (Throwable t){ // parent of all exceptions
+            System.out.println(t.getClass().getSimpleName());
+            System.out.println("Reason + " + t.getMessage() );
+        }
+
+    }
+}
