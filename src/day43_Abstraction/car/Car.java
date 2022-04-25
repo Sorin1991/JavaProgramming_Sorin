22c7d698179ebf26baed248af20eb429733743af
Sorin1991 <basco91@gmail.com>
2022-04-25 18:38:23 +0100

    Car

--- src/day43_Abstraction/car/Car.java
+++ src/day43_Abstraction/car/Car.java
@@ -1,6 +1,6 @@
 package day43_Abstraction.car;
 
-public class Car {
+public abstract class Car {
 
     private final String brand, model;
     private String color;
@@ -50,6 +50,21 @@ public class Car {
         this.price = price;
     }
 
+    public void stop(){
+        System.out.println("Press the brake");
+    }
+
+    public abstract void start();
 
 
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                ", year=" + year +
+                ", price=" + price +
+                '}';
+    }
 }
