d2303485a328edbfef43c883f5682f97ef45c66c
Sorin1991 <basco91@gmail.com>
2022-05-01 18:34:02 +0100

    AnimalTask day44

--- src/day44_AbstractionContinue/animalTask/Animal.java
+++ src/day44_AbstractionContinue/animalTask/Animal.java
@@ -27,7 +27,6 @@ public abstract class Animal {
 
     static{
         canBreathe = true;
-
     }
 
     public Animal(String name, String breed, char gender, int age, String size, String color) {
