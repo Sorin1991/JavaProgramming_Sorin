f2c206d773e997c5d0f2d58c9814c4928717dcc9
Sorin1991 <basco91@gmail.com>
2022-05-12 21:35:31 +0100

    Day 47 PolymorphismContinue

---
+++ src/Day47_Polymorphism_Continue/ReferenceTypeCastings.java
@@ -0,0 +1,21 @@
+package Day47_Polymorphism_Continue;
+
+import day45_Abstraction_Interface.shape.Circle;
+import day45_Abstraction_Interface.shape.Shape;
+
+public class ReferenceTypeCastings {
+
+    public static void main(String[] args) {
+
+
+       // Shape shape = (Shape)  new Circle(4);  // upcasting
+
+        Shape shape2 = new Circle(5);
+
+
+
+
+    }
+
+
+}
