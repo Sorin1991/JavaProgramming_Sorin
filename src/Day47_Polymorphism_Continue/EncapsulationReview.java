6f45c0abb9e4282d212dcd3a337c1dc85170657d
Sorin1991 <basco91@gmail.com>
2022-05-13 08:13:43 +0100

    EncapsulationReview

--- src/Day47_Polymorphism_Continue/EncapsulationReview.java
+++ src/Day47_Polymorphism_Continue/EncapsulationReview.java
@@ -2,7 +2,29 @@ package Day47_Polymorphism_Continue;
 
 public class EncapsulationReview {
 
+    private String bookTitle;
+    private final String publishDate;
 
+    public EncapsulationReview(String bookTitle, String publishDate) {
+        setBookTitle(bookTitle);
+        this.publishDate = publishDate;
+    }
 
+    public String getBookTitle() {
+        if(bookTitle== null){
+            return "";
+        }
+        return bookTitle;
+    }
 
+    public void setBookTitle(String bookTitle) {
+        if(bookTitle == null || bookTitle.isEmpty()){
+            throw new RuntimeException("Invalid Data");
+        }
+        this.bookTitle = bookTitle;
+    }
+
+    public String getPublishDate() {
+        return publishDate;
+    }
 }
