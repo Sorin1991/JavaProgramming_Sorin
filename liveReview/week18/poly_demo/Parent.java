daf78d590a433203862fd4bb3f0232aeb252ce81
Sorin1991 <basco91@gmail.com>
2022-05-14 16:28:36 +0100

    Week18

---
+++ liveReview/week18/poly_demo/Parent.java
@@ -0,0 +1,13 @@
+package week18.poly_demo;
+
+public abstract class Parent {
+
+    public abstract void raiseKid(); // no implementation
+    public abstract void playWithKId();
+    public abstract void feedKid();
+
+
+
+
+
+}
